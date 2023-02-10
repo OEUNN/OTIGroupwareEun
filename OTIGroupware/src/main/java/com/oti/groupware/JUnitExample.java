bef5b69b556d4081e37be039a1b6a7e230a2ddef
ztrl <ztrl@naver.com>
2023-02-10 17:49:50 +0900

    add JUnitExample

---
+++ OTIGroupware/src/main/java/com/oti/groupware/JUnitExample.java
@@ -0,0 +1,13 @@
+package com.oti.groupware;
+
+public class JUnitExample {
+	
+	public int sum(int param1, int param2) {
+		return param1 + param2;
+	}
+	
+	public int minus(int param1, int param2) {
+		return param1 - param2;
+	}
+
+}
