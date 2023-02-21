17201938e6250eb4d8972108af2fde4a5d0d4327
OEUNN <jyejyp5@gmail.com>
2023-02-21 15:43:39 +0900

    2021 final board mail jsp update

--- OTIGroupware/src/main/webapp/resources/js/write-template.js
+++ OTIGroupware/src/main/webapp/resources/js/write-template.js
@@ -1,7 +1,7 @@
 tinymce.init({
 	selector: '#write',
-	width: '355mm',
-	height: '150mm',
+	width: '100%',
+	height: '200mm',
 	theme: 'silver',
 	toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
 	toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
