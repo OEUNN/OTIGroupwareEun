afd41f58f230f0ae59be3a1c0aab8783c43f9711
OEUNN <jyejyp5@gmail.com>
2023-02-28 20:24:11 +0900

    merge

--- src/main/java/com/oti/groupware/employee/dao/EmployeeDAO.java
+++ src/main/java/com/oti/groupware/employee/dao/EmployeeDAO.java
@@ -4,6 +4,8 @@ import com.oti.groupware.employee.dto.Employee;
 
 public interface EmployeeDAO {
     public Employee getEmployeeById(String empId);
+    public void updateLoginFailCnt(Employee employee);
+    public void updateLoginSuccessCnt(Employee employee);
     public int insertEmployee(Employee employee);
     public int updateEmployee(Employee employee);
     public int deleteEmployee(String empId);
