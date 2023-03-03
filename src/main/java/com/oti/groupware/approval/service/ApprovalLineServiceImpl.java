4d6239ee35b7d919cea194ac07ca0d63239db90d
ztrl <ztrl@naver.com>
2023-03-03 09:24:55 +0900

    fix error

--- src/main/java/com/oti/groupware/approval/service/ApprovalLineServiceImpl.java
+++ src/main/java/com/oti/groupware/approval/service/ApprovalLineServiceImpl.java
@@ -3,10 +3,12 @@ package com.oti.groupware.approval.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.oti.groupware.approval.dao.ApprovalLineDAO;
 import com.oti.groupware.approval.dto.ApprovalLine;
 
+@Service
 public class ApprovalLineServiceImpl implements ApprovalLineService{
 	List<ApprovalLine> approvalLines;
 	ApprovalLine approvalLine;
